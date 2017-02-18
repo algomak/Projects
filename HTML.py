c1e8177a5753abc31a6d3fa05427d16706e7f67c
Mehtab93 <khanmehtab.iiit@gmail.com>
2017-02-18 14:45:24 +0530

    preprocessing

---
+++ HTML.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+import os
+data_root="../../cleantext"
+corpus_root="../../dataset/EPS"
+#filename="20021108.txt"
+for filename in os.listdir(corpus_root):
+	file = open(os.path.join(corpus_root, filename), 'r')
+	extract=open(os.path.join(data_root,filename),'w+')
+
+#corpus={}
+#cleantext=file.read()
+#BeautifulSoup is used to remove html tags
+	cleantext = BeautifulSoup(file).text
+#now some comments still there this, function removes them too
+	import re
+
+	def cleanhtml(raw_html):
+	  cleanr = re.compile(r'<[^>]+>')
+	  cleantext = re.sub(cleanr, '', raw_html)
+	  return cleantext
+	cleantext=cleanhtml(cleantext)
+	#print(cleantext)
+
+	extract.write(cleantext)
+	# print(cleantext)
